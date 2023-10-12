967c812a0d7a62b189d011c732c88348dcb1d3c4
JosephEJohnston <1193347519@qq.com>
2023-10-12 14:27:55 +0800

    侧边项目tab跳转实现

--- src/component/side/project-menu/ProjectMenuItemList.tsx
+++ src/component/side/project-menu/ProjectMenuItemList.tsx
@@ -1,34 +1,54 @@
+import MenuItem from "@/component/side/MenuItem";
 
-function ProjectMenuItem() {
+interface ProjectMenuItemProps {
+    id: number;
+    name: string;
+    count: number;
+}
+
+function ProjectMenuItem({id, name, count}: ProjectMenuItemProps) {
     return (
         <>
-            <div className={`function-btn-container`}>
+            <MenuItem key={id} path={`/dashboard/project/${id}`} >
                 <button className="f-btn">
                     <i className="fb-dot bi bi-record-fill"></i>
-                    <span className="f-btn-text">我的工作</span>
+                    <span className="f-btn-text">{name}</span>
                 </button>
                 <div className="f-btn-count-container">
                     <button className="f-btn-count-btn">
                         <i className="bi bi-three-dots"></i>
                     </button>
-                    <div className="f-btn-count">6</div>
+                    <div className="f-btn-count">{count}</div>
                 </div>
-            </div>
+            </MenuItem>
         </>
     )
 }
 
-interface ProjectMenuItemProps {
+const initMenuItemList = [
+    {
+        id: 1,
+        name: '我的工作1',
+        count: 6,
+    },
+    {
+        id: 2,
+        name: '我的工作2',
+        count: 7,
+    },
+];
+
+interface ProjectMenuItemListProps {
     tabOnShow: boolean
 }
 
-export default function ProjectMenuItemList ({tabOnShow}: ProjectMenuItemProps) {
+export default function ProjectMenuItemList ({tabOnShow}: ProjectMenuItemListProps) {
     return (
         <>
             <div className={`project-task-container 
                     ${tabOnShow ? '' : 'project-task-container-not-show'}`}>
-                <ProjectMenuItem />
-                <ProjectMenuItem />
+                {initMenuItemList.map(item =>
+                    <ProjectMenuItem id={item.id} name={item.name} count={item.count}/>)}
             </div>
         </>
     );
