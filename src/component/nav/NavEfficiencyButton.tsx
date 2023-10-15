dc7f174ad80589af2c8e7f9bd55c40ee41d2ef45
JosephEJohnston <1193347519@qq.com>
2023-10-15 22:12:58 +0800

    微调

--- src/component/nav/NavEfficiencyButton.tsx
+++ src/component/nav/NavEfficiencyButton.tsx
@@ -1,6 +1,4 @@
-'use client'
-
-import {Dispatch, SetStateAction, useState} from "react";
+import {Dispatch, SetStateAction} from "react";
 
 interface NavEfficiencyButtonProps {
     show: boolean,
