829abc9a6ac217d674f3fb1681d53f2ecf256425
JosephEJohnston <1193347519@qq.com>
2023-10-16 12:48:34 +0800

    效率框，大部分实现

--- src/component/nav/FixPositionEfficiencyBoard.tsx
+++ src/component/nav/FixPositionEfficiencyBoard.tsx
@@ -13,8 +13,28 @@ export default function FixPositionEfficiencyBoard (props: FixPositionEfficiency
             return (
                 <>
                     <div className={`nav-efficiency-board`}>
+                        <div className={`eff-board-title`}>
+                            <span className={`eff-board-title-text`}>您的效率</span>
+                        </div>
                         <div>
-                            您的效率
+                            <img className={`efficiency-img`} src="images/efficiency.jpg" alt="#"/>
+                        </div>
+                        <div className={`eff-board-desc-container`}>
+                            <div className={`eff-board-desc-item eff-board-desc-title`}>
+                                <span className={`eff-board-desc-title-text`}>查看一点一滴的进步</span>
+                            </div>
+                            <div className={`eff-board-desc-item`}>
+                                <span className={`eff-board-desc-text`}>设置每日和每周目标，回顾您的效率轨迹，并见证Karma分数的增长。</span>
+                            </div>
+                            <button className={`eff-board-desc-item eff-board-btn`}>
+                                设置您的目标
+                            </button>
+                            <div className={`eff-board-desc-item eff-board-more`}>
+                                <i className="bi bi-question-circle"></i>
+                                <span  className={`eff-board-more-text`}>
+                                    了解更多有关Karma的详细内容
+                                </span>
+                            </div>
                         </div>
                     </div>
                 </>
